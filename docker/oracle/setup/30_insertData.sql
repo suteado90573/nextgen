1570166b71adac6ef74f9ea42837a65208568c21
高木　大介 <takagi-d@iij.ad.jp>
2024-07-16 18:34:44 +0900

    issue #30 docker配下にoracle資材を追加。

---
+++ docker/oracle/setup/30_insertData.sql
@@ -0,0 +1,14 @@
+-- 初期データ投入のSQLを記述する
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-01', 'event-01', 'type-01', 'position-01', 'place-01');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-02', 'event-02', 'type-02', 'position-02', 'place-02');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-03', 'event-03', 'type-03', 'position-03', 'place-03');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-04', 'event-04', 'type-04', 'position-04', 'place-04');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-05', 'event-05', 'type-05', 'position-05', 'place-05');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-06', 'event-06', 'type-06', 'position-06', 'place-06');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-07', 'event-07', 'type-07', 'position-07', 'place-07');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-08', 'event-08', 'type-08', 'position-08', 'place-08');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-09', 'event-09', 'type-09', 'position-09', 'place-09');
+INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES('stand-10', 'event-10', 'type-10', 'position-10', 'place-10');
+COMMIT;
+
+-- SELECT COUNT(*) FROM SEAT;
