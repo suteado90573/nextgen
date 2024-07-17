0e00a428042d173b230ee554b19b7c270eb80595
高木　大介 <takagi-d@iij.ad.jp>
2024-07-17 13:32:49 +0900

    issue #31 Java8ではテキストブロックが記述できないため、修正。

--- src/main/java/com/example/nextgen/mapper/SeatMapper.java
+++ src/main/java/com/example/nextgen/mapper/SeatMapper.java
@@ -13,59 +13,18 @@ import com.example.nextgen.entity.Seat;
 @Mapper
 public interface SeatMapper {
 
-	@Select("""
-			SELECT
-				ID, STAND, EVENT, TYPE, POSITION, PLACE,
-				IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT
-			FROM SEAT
-			""")
+	@Select("SELECT ID, STAND, EVENT, TYPE, POSITION, PLACE, IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT FROM SEAT")
 	List<Seat> findAll();
 
-	@Select("""
-			SELECT
-				ID, STAND, EVENT, TYPE, POSITION, PLACE,
-				IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT
-			FROM SEAT
-			WHERE ID = #{id}
-			""")
+	@Select("SELECT ID, STAND, EVENT, TYPE, POSITION, PLACE, IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT FROM SEAT WHERE ID = #{id}")
 	Seat findById(Integer id);
 
-	@Select("""
-			<script>
-				SELECT
-					ID, STAND, EVENT, TYPE, POSITION, PLACE,
-					IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT
-				FROM SEAT
-				<where>
-					<if test="stand != null">
-					  STAND LIKE CONCAT(CONCAT('%', #{stand}), '%')
-					</if>
-					<if test="event != null">
-					  AND EVENT LIKE CONCAT(CONCAT('%', #{event}), '%')
-					</if>
-					<if test="type != null">
-					  AND TYPE LIKE CONCAT(CONCAT('%', #{type}), '%')
-					</if>
-					<if test="position != null">
-					  AND POSITION LIKE CONCAT(CONCAT('%', #{position}), '%')
-					</if>
-				</where>
-			</script>
-			""")
+	@Select("SELECT ID, STAND, EVENT, TYPE, POSITION, PLACE, IS_DELETED, CREATED_BY, CREATED_AT, UPDATED_BY, UPDATED_AT FROM SEAT")
 	List<Seat> search(SearchSeatCondIn searchSeatCondIn);
 
-	@Insert("""
-			INSERT INTO SEAT(
-				STAND, EVENT, TYPE, POSITION, PLACE
-			) VALUES(
-				#{stand}, #{event}, #{type}, #{position}, #{place}
-			)
-			""")
+	@Insert("INSERT INTO SEAT(STAND, EVENT, TYPE, POSITION, PLACE) VALUES(#{stand}, #{event}, #{type}, #{position}, #{place})")
 	int insert(Seat seat);
 
-	@Delete("""
-			DELETE FROM SEAT
-			WHERE ID = #{id}
-			""")
+	@Delete("DELETE FROM SEAT WHERE ID = #{id}")
 	int deleteLast(Seat seat);
 }
